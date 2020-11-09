eec555673004dec269d66262cc27e440a85cf868
tomoya0605 <t.tomoya0605@gmail.com>
2020-11-10 01:39:16 +0900

    デプロイ,Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # before_action :basic_auth
+
+  private
+
+  def basic_auth
+    # 'admin'というユーザー名と、'password'というパスワードでBasic認証できるように設定
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'tomoya' && password == 'tomomari0605'
+    end
+  end
 end
