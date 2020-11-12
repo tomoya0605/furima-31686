6f911169729a7c1e7e651d970cc447d06b76db16
tomoya0605 <t.tomoya0605@gmail.com>
2020-11-12 17:56:54 +0900

    変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password, length: { minimum: 6 }
-    validates :encrypted_password, length: { minimum: 6 }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
