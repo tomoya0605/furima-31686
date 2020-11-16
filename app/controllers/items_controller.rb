63ee732babb3be69bc7327c7f6faebf679be52b2
tomoya0605 <t.tomoya0605@gmail.com>
2020-11-16 09:26:14 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   before_action :move_to_index, only: [:edit]
 
-
   def index
     @items = Item.order('id DESC')
   end
@@ -26,10 +25,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
@@ -47,9 +45,8 @@ class ItemsController < ApplicationController
       :area_id, :number_of_month_id, :price
     ).merge(user_id: current_user.id)
   end
+
   def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
