04f09be36c20facba9d5c6d85adc961847d7e8b1
tomoya0605 <t.tomoya0605@gmail.com>
2020-11-21 20:47:34 +0900

    変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless current_user.id == @item.user_id
+    redirect_to action: :index if current_user.id != @item.user_id || @item.purchaser_history.present?
   end
 
   def update
